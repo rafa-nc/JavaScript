5e280b3a81ad09402b3cc1fdfd4fd7a497e63c76
Rafael Costa <rafael.costa04@outlook.com>
2022-03-24 01:44:21 -0300

    tipos de variaveis

--- Strings.js
+++ Strings.js
@@ -3,6 +3,20 @@ const texto1 = "xD";
 console.log(texto1)
 
 const cidade = "belo horizonte";
-const input = "Belo Horizonte";
 
-console.log(cidade + input);
+
+console.log(cidade)
+
+let input = 0;
+
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10;
+console.log(operacao);
---
+++ variaveis.js
@@ -0,0 +1,40 @@
+//var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// var area;
+// area =  altura * comprimento;
+// console.log(area)
+
+//let
+
+// let area;
+// let forma = 'retangulo'
+// let altura = 6;
+// let comprimento = 7;
+
+// if (forma === 'retangulo') {
+//     area = altura * comprimento;
+
+// } else {
+//     area  = (altura * comprimento) / 2;
+// }
+
+// console.log(area);
+
+let area;
+const forma = 'retangulo'
+const texto1 = 'retangulo'
+const texto2 = 'triangulo'
+const altura = 6;
+const comprimento = 7;
+
+if (forma === 'retangulo') {
+    area = altura * comprimento;
+    console.log(texto1)
+} else {
+    area  = (altura * comprimento) / 2;
+    console.log(texto2)
+}
+ console.log(area)
